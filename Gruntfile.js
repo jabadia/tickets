ca4788a3d86c7f6bd797a077f8dd004926db85e5
Javier Abadia <javi.abadia@gmail.com>
2014-02-06 15:28:47 +0100

    grunt para generar el fichero manifest

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt)
+{
+	grunt.loadNpmTasks('grunt-manifest');
+	grunt.initConfig({
+		manifest: {
+			generate: {
+				options: {
+					basePath: '.',
+					cache: [
+						'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js',
+						'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js',
+						'//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js',
+						'//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css',
+						'//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css'
+					],
+					network: [],
+					fallback: [],
+					verbose: true,
+					master: 'index.html',
+					timestamp: true,
+					hash: true
+				},
+				src: [
+				'index.html'
+				],
+				dest: 'tickets.manifest'
+			}
+		}
+	});
+
+	grunt.registerTask('default',['manifest']);
+};
